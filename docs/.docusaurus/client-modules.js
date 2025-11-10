8b4acca834dc66c76482faa5d079a7e898ae338a
roib20 <66280613+roib20@users.noreply.github.com>
2025-11-10 19:36:02 +0200

    refactor: remove `docs/.docusaurus`
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/roib/github/CALMe/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/roib/github/CALMe/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/roib/github/CALMe/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/roib/github/CALMe/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/roib/github/CALMe/docs/src/css/custom.css"),
-];
